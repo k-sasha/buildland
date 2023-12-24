2f04de91a596dcb49c6c9baa00a9ca1fc3166fe2
Anisimova Aleksandra <a.y.kirgizova@gmail.com>
2023-12-24 06:32:43 -0800

    Created Manufacturer class

---
+++ src/main/java/com/sasha/buildland/entity/Manufacturer.java
@@ -0,0 +1,39 @@
+package com.sasha.buildland.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@Table(name = "manufacturers")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Manufacturer implements InlineKeyboardObject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "manufacturerSeq")
+    @SequenceGenerator(name = "manufacturerSeq", sequenceName = "manufacturer_seq", allocationSize = 1)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "manufacturer")
+    private String manufacturerName;
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getName() {
+        return this.manufacturerName;
+    }
+}
